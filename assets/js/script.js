4005c356a7fa6c2c11ac641a3297acb7dc74e557
kirbyfridge <114848971+kirbyfridge@users.noreply.github.com>
2022-10-25 19:37:40 -0400

    made changes to properly fix javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,5 @@ var articlesDiv = document.getElementById('articles');
 var headerDiv = document.getElementById('header');
 
 // Change style by accessing style object's properties
-articlesDiv.children[1].style.fontSize = '40px';
-headerDiv.style.color = 'blue';
+articlesDiv.children[0].style.fontSize = '50px';
+headerDiv.children[0].style.color = 'white';
